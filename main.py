d83f61df92af013c5a2e6be05a5532c75bf38d08
Sarveshwaran Dhanasekar <sarveshwaran.dk@gmail.com>
2018-10-10 10:03:36 -0400

    Updating branch with suhitk_local

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ def parse_arguments():
 	parser.add_argument('--batch_size', dest='bsz', type=int, default=32)
 	parser.add_argument('--episodes', dest='epi', type=int, default=2000)
 	parser.add_argument('--test_episodes', dest='test_epi', type=int, default=100)
-	parser.add_argument('--eps_init', dest='greedy_epsilon_init', type=int, default=0.5)
-	parser.add_argument('--eps_stop', dest='greedy_epsilon_stop', type=int, default=0.05)
-	parser.add_argument('--eps_iter', dest='greedy_epsilon_iter', type=int, default=10000)
+	parser.add_argument('--eps_init', dest='epsilon_init', type=int, default=0.5)
+	parser.add_argument('--eps_stop', dest='epsilon_stop', type=int, default=0.05)
+	parser.add_argument('--eps_iter', dest='epsilon_iter', type=int, default=10000)
+	parser.add_argument('--eps_greedy', dest='greedy_epsilon', type=int, default=0.05)
 	return parser.parse_args()
 
 
